3ee3be723e6f066055c2dabd2e67ae261831a12e
Matthew Flaherty <matthewflaherty77@hotmail.com>
2023-08-04 17:47:00 -0400

    Base Website
    Nav Bar and logo included

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//images
+import Logo from '../assets/Logo.jpg'
+
+const Header = () => {
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          {/* logo */}
+          <a href='#'>
+            <img src={Logo} width={100} height={100}></img>
+          </a>
+          {/* button */}
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
